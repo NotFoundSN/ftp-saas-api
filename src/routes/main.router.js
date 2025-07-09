825c1450348513f367573dc20cdc773a1511d871
CoioteSN <julio.maldonado.sn@gmail.com>
2025-07-09 15:46:48 -0300

    fix: ⚡️ main route

--- src/routes/main.router.js
+++ src/routes/main.router.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import middleware from "../middleware/auth.middleware.js";
 import fileRouter from "./file.router.js";
-import multer from 'multer';
+import multer from "multer";
 const upload = multer();
 //import userRouter from "./user.router.js";
 //import cartRouter from "./carts.router.js";
 
 const router = Router();
+
+router.get("/", (req, res) => {
+	res.status(200).json({
+		message: "Welcome to the FTP SaaS API",
+	});
+});
 
 router.use("/file", upload.array("file"), middleware, fileRouter);
 
